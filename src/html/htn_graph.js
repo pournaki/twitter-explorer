39be6adfc158c6e2585cab815eaae33ccf5888c0
pournaki <pournaki@mis.mpg.de>
2020-10-12 20:15:31 +0200

    Add new feature: text as nodes in hashtag network

--- src/html/htn_graph.js
+++ src/html/htn_graph.js
@@ -1,19 +1,31 @@
 <script>
 var colorselector = document.getElementById("nodecolor");
-var nodescaling = 0.1;
+var nodescaling = 3;
       var nodecolor = colorselector.options[colorselector.selectedIndex].value;
       const elem = document.getElementById('graph');
       const Graph = ForceGraph()(elem)
       .graphData({nodes: data.nodes, links: data.links})
       .nodeColor(node => "black")
-      .nodeId('name')
-      .nodeLabel(node => '#'+node.name)
-      .nodeVal(node => node.degree * nodescaling)
-      .linkDirectionalParticleColor(() => 'red')
-      .linkHoverPrecision(10)
-      .linkCurvature(0.01)
-      .onNodeClick(node => $("#content03").slideDown(300))
+      .nodeLabel("")
+      .nodeCanvasObject((node, ctx, globalScale) => {
+          const label = node.name;
+          const fontSize = Math.log(node.degree) * nodescaling
+          ctx.font = `${fontSize}px Sans-Serif`;
+          const textWidth = ctx.measureText(label).width;
+          const bckgDimensions = [textWidth, fontSize].map(n => n + fontSize * 0.1); // some padding
 
+          ctx.fillStyle = 'rgba(255, 255, 255, 0.8)';
+          ctx.fillRect(node.x - bckgDimensions[0] / 2, node.y - bckgDimensions[1] / 2, ...bckgDimensions);
+
+          ctx.textAlign = 'center';
+          ctx.textBaseline = 'middle';
+          ctx.fillStyle = "black";
+          ctx.fillText("#"+label, node.x, node.y);
+        }) 
+      .nodeId('name')
+      .linkHoverPrecision(10)
+      .linkColor(link => 'rgba(0,0,0,0.1)')
+      .onNodeClick(node => {highlight(node)})
       .onNodeRightClick(node => { Graph.centerAt(node.x, node.y, 1000);Graph.zoom(8, 2000);})
 
       // Get the list of all users for autocomplete
@@ -23,12 +35,88 @@ var nodescaling = 0.1;
 
       var name = document.getElementById("searchuser").value;
 
+                function highlight(node){
+            $("#content03").slideDown(300);
+            pastenodeinfo(node);
+            var neighbors = []
+            neighbors.push(node)
+
+            for (link of data.links) {
+              if (link.source == node) {
+                neighbors.push(link.target)
+                link.colorthat = 1
+              }
+              else if (link.target == node){
+                neighbors.push(link.source)
+                link.colorthat = 1
+              }
+              else {link.colorthat=0}
+            }
+            for (node of data.nodes){
+              if(neighbors.includes(node)){
+                node.colorthat = 1
+              }
+              else node.colorthat = 0}
+            colorbar = ['#d3d3d3', 'red']
+            var nodescaleslider = document.getElementById("slido");
+            var newscale = nodescaleslider.value  
+          
+            Graph.nodeCanvasObject((node, ctx, globalScale) => {
+                  const label = node.name;
+                  const fontSize = Math.log(node.degree) * newscale
+                  ctx.font = `${fontSize}px Sans-Serif`;
+                  const textWidth = ctx.measureText(label).width;
+                  const bckgDimensions = [textWidth, fontSize].map(n => n + fontSize * 0.1); // some padding
+
+                  ctx.fillStyle = 'rgba(255, 255, 255, 1.0)';
+                  ctx.fillRect(node.x - bckgDimensions[0] / 2, node.y - bckgDimensions[1] / 2, ...bckgDimensions);
+
+                  ctx.textAlign = 'center';
+                  ctx.textBaseline = 'middle';
+                  ctx.fillStyle = colorbar[node.colorthat];
+                  ctx.fillText("#"+label, node.x, node.y);
+                })
+            // linkcolor depending on dark/lightmode
+            var bodyelement = document.querySelector('body')
+            var bodystyle = window.getComputedStyle(bodyelement)
+            var bg = bodystyle.getPropertyValue('color')
+            if (bg === 'rgb(0, 0, 0)') {var colorbar2 = ['rgba(0,0,0,0.05)', 'rgba(255,0,0,0.5)']}
+            if (bg === 'rgb(255, 255, 255)') {var colorbar2 = ['rgba(255,255,255,0.03)', 'rgba(255,0,0,0.5)']}
+            Graph.linkColor(link => colorbar2[link.colorthat])
+            }
+            Graph.onBackgroundClick(() => resetcolors())
+
+            function resetcolors(){
+            var colorselector = document.getElementById("nodecolor");
+            var selectedoption = colorselector.options[colorselector.selectedIndex].value
+    
+            var nodescaleslider = document.getElementById("slido");
+            var newscale = nodescaleslider.value  
+            
+                Graph.nodeCanvasObject((node, ctx, globalScale) => {
+                  const label = node.name;
+                  const fontSize = Math.log(node.degree) * newscale
+                  ctx.font = `${fontSize}px Sans-Serif`;
+                  const textWidth = ctx.measureText(label).width;
+                  const bckgDimensions = [textWidth, fontSize].map(n => n + fontSize * 0.1); // some padding
+
+                  ctx.fillStyle = 'rgba(255, 255, 255, 1.0)';
+                  ctx.fillRect(node.x - bckgDimensions[0] / 2, node.y - bckgDimensions[1] / 2, ...bckgDimensions);
+
+                  ctx.textAlign = 'center';
+                  ctx.textBaseline = 'middle';
+                if (selectedoption == "louvain_com" ) {ctx.fillStyle = colorscale[node["louvain_com"]];}
+                else {ctx.fillStyle = "black";}
+                  ctx.fillText("#"+label, node.x, node.y);
+                })
+                Graph.linkColor(link => 'rgba(0,0,0,0.1)')
+                }
 var input = document.getElementById("searchuser");
 new Awesomplete(input, {
 list: users
   });
 var name = document.getElementById("searchuser").value;
-console.log(name);
+
 // var searcheduser = document.getElementById("searchuser").value;
 
 function zoomonuser(){
@@ -38,35 +126,7 @@ function zoomonuser(){
   }
   var nodeathand = getNode(name)
   Graph.centerAt(nodeathand.x, nodeathand.y, 1000);Graph.zoom(8, 2000);
-  console.log(nodeathand);}
-
-function flashcolor(){
-  var bodyelement = document.querySelector('body')
-  var bodystyle = window.getComputedStyle(bodyelement)
-  var bg = bodystyle.getPropertyValue('color')
-  if (bg === 'rgb(0, 0, 0)') {var nodecol = 'black'}
-  if (bg === 'rgb(255, 255, 255)') {var nodecol = 'white'}
-  var name = document.getElementById("searchuser").value;
-  const getNode = id => {
-      return data.nodes.find(node => node.name === name);
-  };
-  var nodeathand = getNode(name)
-  console.log(nodeathand)
-  originalcolor = nodeathand.color
-  Graph.nodeColor(node => {
-    if (node.name === name) {
-      return "red";
-        }
-    else {return nodecol}
-          });
-  setTimeout(function(){            
-    Graph.nodeColor(node => {
-    if (node.name === name) {
-      return nodecol;
-        }
-    else {return nodecol}
-          });
-    }, 250);}
+  }
 
 function resetzoom(){
   Graph.centerAt(0, 0,1000);Graph.zoom(0.8, 1000)
@@ -99,19 +159,44 @@ let trans = () => {
 }
 
 var colorscale = ['#e6194b', '#3cb44b', '#ffe119', '#4363d8', '#f58231', '#911eb4', '#46f0f0', '#f032e6', '#bcf60c', '#fabebe', '#008080', '#e6beff', '#9a6324', '#fffac8', '#800000', '#aaffc3', '#808000', '#ffd8b1', '#000075', '#808080', '#ffffff', '#000000']
+
+function changecolor(bytype) {
+
+var nodescaleslider = document.getElementById("slido");
+var newscale = nodescaleslider.value  
+
+Graph.nodeCanvasObject((node, ctx, globalScale) => {
+      const label = node.name;
+      const fontSize = Math.log(node.degree) * newscale
+      ctx.font = `${fontSize}px Sans-Serif`;
+      const textWidth = ctx.measureText(label).width;
+      const bckgDimensions = [textWidth, fontSize].map(n => n + fontSize * 0.1); // some padding
+
+      ctx.fillStyle = 'rgba(255, 255, 255, 1.0)';
+      ctx.fillRect(node.x - bckgDimensions[0] / 2, node.y - bckgDimensions[1] / 2, ...bckgDimensions);
+
+      ctx.textAlign = 'center';
+      ctx.textBaseline = 'middle';
+      if (bytype == "louvain"){ctx.fillStyle = colorscale[node["louvain_com"]]}
+      else {ctx.fillStyle = "black"}
+      ;
+      ctx.fillText("#"+label, node.x, node.y);
+    })}
+
 document.getElementById("nodecolor").addEventListener("change", recolornodes);
 function recolornodes(){
+
   var colorselector = document.getElementById("nodecolor");
   var selectedoption = colorselector.options[colorselector.selectedIndex].value
   if (selectedoption != "none"){
-  Graph.nodeColor(node => colorscale[node[selectedoption]])}            
+  changecolor("louvain")}            
   else { 
   var bodyelement = document.querySelector('body')
   var bodystyle = window.getComputedStyle(bodyelement)
   var bg = bodystyle.getPropertyValue('color')
   if (bg === 'rgb(0, 0, 0)') {var nodecol = 'black'}
   if (bg === 'rgb(255, 255, 255)') {var nodecol = 'white'}
-  Graph.nodeColor(node => nodecol)} }
+  changecolor("none")} }
 
 function openhashtag(){ 
   var hashtagname = document.getElementById("searchuser").value;
@@ -121,20 +206,29 @@ function openhashtag(){
 // NODE SIZE
 document.getElementById("slido").addEventListener("change", rescalenodes);
 function rescalenodes(){
+  var colorselector = document.getElementById("nodecolor");
+  var selectedoption = colorselector.options[colorselector.selectedIndex].value
   var nodescaleslider = document.getElementById("slido");
   var newscale = nodescaleslider.value  
-  var sizeselector = document.getElementById("nodesize");
-  var selectedoption = sizeselector.options[sizeselector.selectedIndex].value
-  if (selectedoption === "degree"){Graph.nodeVal(node => node[selectedoption] * 0.05 * newscale)}
-  else { Graph.nodeVal(node => 1.0 * newscale)}            
-}
+  Graph.nodeCanvasObject((node, ctx, globalScale) => {
+    const label = node.name;
+    const fontSize = Math.log(node.degree) * newscale
+    ctx.font = `${fontSize}px Sans-Serif`;
+    const textWidth = ctx.measureText(label).width;
+    const bckgDimensions = [textWidth, fontSize].map(n => n + fontSize * 0.1); // some padding
 
-document.getElementById("nodesize").addEventListener("change", changenodesize);
-function changenodesize(){
-var sizeselector = document.getElementById("nodesize");
-var selectedoption = sizeselector.options[sizeselector.selectedIndex].value
-if (selectedoption === "degree"){Graph.nodeVal(node => node[selectedoption] * 0.05)}            
-else { Graph.nodeVal(node => 1.0)} 
+    ctx.fillStyle = 'rgba(255, 255, 255, 1.0)';
+    ctx.fillRect(node.x - bckgDimensions[0] / 2, node.y - bckgDimensions[1] / 2, ...bckgDimensions);
+
+    ctx.textAlign = 'center';
+    ctx.textBaseline = 'middle';
+
+    if (selectedoption == "louvain_com" ) {ctx.fillStyle = colorscale[node["louvain_com"]];}
+    else {ctx.fillStyle = "black";}
+    
+    ctx.fillText("#"+label, node.x, node.y);
+  })  
+
 }
 
 $(function() {
@@ -149,7 +243,6 @@ userinfostring = `<ul>
 </ul>`
 document.getElementById('userinfostring').innerHTML = userinfostring
 document.getElementById("searchuser").value = node.name}
-Graph.onNodeHover(node => node && pastenodeinfo(node))
 
 var netinfo = `<ul> 
 <li> Keyword: ${data.graph.keyword}</li>
