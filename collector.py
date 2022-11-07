b3ec6191e9409d4d3e5cbe07b2da61499fad21b1
pournaki <53080134+pournaki@users.noreply.github.com>
2022-11-07 22:29:26 +0530

    Extend date range for academic API

--- collector.py
+++ collector.py
@@ -76,7 +76,10 @@ if api_type in ['Standard API v2','Academic Research API v2']:
                               )
         timerange = st.date_input(label="DATE_RANGE/ Return tweets created in the given time range (including the first and last day).",
                                   help="Keep in mind that the search index has a 7-day limit if you are not using the academic research API: choosing a date earlier than one week past will return no tweets.",
-                                  value=[datelastweek,datetoday])    
+                                  value=[datelastweek,datetoday],
+                                  min_value=datetime.date(2016,7,15), # day Twitter was launched
+                                  max_value=datetoday
+                                  )    
         since_date = timerange[0]
         ## until the full last day
         until_date = timerange[1] + datetime.timedelta(days=1)    
