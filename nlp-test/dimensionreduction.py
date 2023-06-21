a20cb49c4126629cd22a7edf9696fab7b7a22289
Felix Grandner <felixgrandner@gmx.net>
2023-06-21 20:09:28 +0200

    polish bugfix

--- nlp-test/dimensionreduction.py
+++ nlp-test/dimensionreduction.py
@@ -1,17 +1,39 @@
 import sys
 sys.path.append("/home/felix/Documents/bif/inno2/twitter-explorer/")
 
+import os
 import pandas as pd
 import numpy as np
+import altair as alt
+import streamlit as st
+import datetime as dt
+import spacy
 import time
 
 from HanTa import HanoverTagger as ht
+from sklearn.decomposition import PCA
+from sklearn.manifold import MDS
+import umap.umap_ as umap
 from sklearn.metrics import silhouette_score
 
+from twitterexplorer.legacy import *
+from twitterexplorer.helpers import *
+from twitterexplorer.streamlitutils import *
+from twitterexplorer.plotting import *
+from twitterexplorer.converters import twitterjsonl_to_twitwicsv
+from twitterexplorer.converters import export_graph
+from twitterexplorer.defaults import *
+from twitterexplorer.constants import *
 from twitterexplorer.__version__ import __version__
 from twitterexplorer.networks import InteractionNetwork
+from twitterexplorer.networks import SemanticNetwork
 
+
+import nltk #Natural Language ToolKit is a library for NLP from Steven Bird, Ewan Klein, and Edward Loper (2009).
 import re #to deal with regular expressions
+import string #A collection of string constants
+
+
 
 def load_data_old(path):
         ## pandas does strange things to IDs, like importing them as ints and using
@@ -69,16 +91,20 @@ df_new = df[['user_id', 'text']]
 df_new2 = df_new.groupby(['user_id']).agg("\n".join)
 
 corpus_original = df_new2['text'].values.tolist()
+
 #print("The text original: \n", corpus_original)
 
 #lower-casing the text
 corpus = list(map(lambda x: x.lower(), corpus_original))
+#print("The text lower case: \n", corpus)
 
-#removing urls
+
 corpus = list(map(lambda x: re.sub(r'https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&\/\/=]*)', '', x), corpus))
+#print("The text without links: \n", corpus)
 
 #removing digits from the text
 corpus = list(map(lambda x: re.sub(r'\d+', '', x), corpus))
+#print("The text without digits: \n", corpus)
 
 #removing punctuation marks
 corpus = list(map(lambda x: re.sub(r"[!\"#$%&'()*+,-./:;<=>?@[\]^_`{|}~]", '', x), corpus))
@@ -86,91 +112,71 @@ corpus = list(map(lambda x: re.sub(r"[!\"#$%&'()*+,-./:;<=>?@[\]^_`{|}~]", '', x
 #removing everything else
 corpus = list(map(lambda x: re.sub(r'[^\w\s]', '', x), corpus))
 
+#print("The text without punctuation marks: \n", corpus)
+
 #removing trailing whitespaces
 corpus = list(map(lambda x: ' '.join([token for token in x.split()]), corpus))
-
 #print("The text: \n", corpus)
 
 
-
+import nltk
 from nltk.tokenize import word_tokenize
 from nltk.corpus import stopwords
 
 # stopwords
+
 stopwords_twitter = stopwords.words("german")
 stopwords_twitter.append("rt")
 stopwords_twitter.append(stopwords.words("english"))
 
 tokenized = list(map(lambda x: word_tokenize(x), corpus))
 tokenized = [[word for word in x if word not in stopwords_twitter] for x in tokenized]
+#print("Stopwordized corpus: \n", tokenized)
 
 # lemmatization
 tagger_de = ht.HanoverTagger('morphmodel_ger.pgz')
 tagger_en = ht.HanoverTagger('morphmodel_en.pgz')
 
 lemmatized = [[tagger_en.analyze(word)[0].lower() for word in x] for x in tokenized]
+#print("After Lemmatization: \n", lemmatized)
 
 # finalized
 final = list(zip(list(df_new2["text"].keys()), lemmatized))
+#print(final)
 
 from sklearn.feature_extraction.text import TfidfVectorizer
-
 tfidf = TfidfVectorizer(tokenizer=lambda i:i, lowercase=False)
 result = tfidf.fit_transform(lemmatized)
 
 final2 = list(zip(list(df_new2["text"].keys()), result))
 #print(final2)
 
+# PCA
 
-# Compare leiden communities to calculated tfidf data
+pca = PCA(n_components=2)
+pca.fit(result.toarray())
+pca2D = pca.transform(result.toarray())
 
-# building twitterexplorer interaction network to calculate leiden communities
-G = InteractionNetwork()
-G.build_network(pandas_dataframe=df,
-                language_filter=None,
-                interaction_type="retweet",
-                starttime=None,
-                endtime=None)
-G.reduce_network(giant_component=True,
-                    aggregation="soft",
-                    hard_agg_threshold=0)
-
-G_comdec = G._graph.copy()
-G_comdec.vs['weight'] = 1
-if 'weight' not in G_comdec.edge_attributes():
-    G_comdec.es['weight'] = 1
-G_comdec = G_comdec.simplify(multiple=True,
-                                loops=True,
-                                combine_edges={'tweetid':'ignore',
-                                            'timestamp':'ignore',
-                                            'weight':'sum'})    
-G_comdec.to_undirected(mode='collapse',combine_edges={'weight':'sum'})
-
-# leiden communities
-partition_leiden = G_comdec.community_leiden(objective_function='modularity') 
-print(partition_leiden.membership) 
+# explained pca variance ratio
+print(pca.explained_variance_ratio_)
 
 
-# build features from tfidf data and labels from leiden communities
-leidenlist = [list(t) for t in final2]
-for x in leidenlist:
-    try:
-        x.append(partition_leiden.membership[leidenlist.index(x)])
-    except:
-        x.append(None)
+# MDS
 
-leidenlist = [item for item in leidenlist if item[2] is not None]
+mds = MDS(n_components=2)
+mds2D = mds.fit_transform(result.toarray())
 
 
-features = np.array([item[1].toarray()[0] for item in leidenlist]) 
-labels = np.array([item[2] for item in leidenlist if item[2] is not None])
+# UMAP
+
+umap_reducer = umap.UMAP()
+umap2D = umap_reducer.fit_transform(result.toarray())
 
 
-# calculate mean silhouette scores for data
-score = silhouette_score(features, labels, metric="euclidean")
-#score = silhouette_score(features, labels, metric="cosine")
 
-print(score)
+import matplotlib.pyplot as plt
+plt.scatter(pca2D[:,0], pca2D[:,1], color="b")
+plt.scatter(mds2D[:,0], mds2D[:,1], color="r")
+plt.scatter(umap2D[:,0], umap2D[:,1], color="g")
 
-end = time.time()
-print("time: ", end-start)
+plt.show()
